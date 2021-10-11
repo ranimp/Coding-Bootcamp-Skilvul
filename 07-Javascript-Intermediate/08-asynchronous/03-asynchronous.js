2149cffa9f666fb76a6114ead28d79b62c7012c1
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2021-10-11 18:28:53 +0800

    tambahan

--- 07-Javascript-Intermediate/08-asynchronous/03-asynchronous.js
+++ 07-Javascript-Intermediate/08-asynchronous/03-asynchronous.js
@@ -4,18 +4,18 @@ const getGitHubUser = async (username) => {
   try {
       if(!username){
       return null
-  }
-  const data = await fetch(`https://api.github.com/users/${username}`);
-  let respon = await data.json()
-  if(respon?.login){
-      return 'Object'
-  }
-  }
-  catch (error) {
-      console.log(error)
-  }
-  return "not found"
-  }
+    }
+    const data = await fetch(`https://api.github.com/users/${username}`);
+    let respon = await data.json()
+    if(respon?.login){
+        return 'Object'
+    }
+    }
+    catch (error) {
+        console.log(error)
+    }
+    return "not found"
+    }
   const printGitHubUser = async () => {
     const mojombo = await getGitHubUser("mojombo");
     const orange = await getGitHubUser("");
